40293b4222bbf641445bc980c2955da138c0b646
hildamu <mukamihilda437@gmail.com>
2024-07-05 13:17:23 +0300

    submit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        response_content = self.get_response_body()
+        return json.loads(response_content)
+
+URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'  
+JSON_STRING = b'[\n  {\n    "name": "Daniel",\n    "occupation": "LG Fridge Salesman"\n  },\n  {\n    "name": "Joe",\n    "occupation": "WiFi Fixer"\n  },\n  {\n    "name": "Avi",\n    "occupation": "DJ"\n  },\n  {\n    "name": "Howard",\n    "occupation": "Mountain Legend"\n  }\n]\n'
+CONVERTED_DATA = [
+    {"name": "Daniel", "occupation": "LG Fridge Salesman"},
+    {"name": "Joe", "occupation": "WiFi Fixer"},
+    {"name": "Avi", "occupation": "DJ"},
+    {"name": "Howard", "occupation": "Mountain Legend"}
+]
+
